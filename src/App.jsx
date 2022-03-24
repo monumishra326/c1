a0c446ba3cd4ada54020bbba39b4d35ae71c883f
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-24 17:26:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,120 @@
-
-import './App.css';
-import { useState } from "react"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const[book,setBook]= useState(13);
-  const[pen,setPen]= useState(10);
-  const[notebook,setNotebook] = useState(44);
-  const[inkpen,setInkpen] = useState(78);
+  const [book, setBook] = useState(13);
+  const [pen, setPen] = useState(10);
+  const [notebook, setNotebook] = useState(44);
+  const [inkpen, setInkpen] = useState(78);
 
-  const bookHandler = (val)=>{
-    setBook(book+val);
+  const bookHandler = (val) => {
+    setBook(book + val);
   };
-  const penHandler = (val)=>{
-   setPen(pen+val);
+  const penHandler = (val) => {
+    setPen(pen + val);
   };
-  const notebookHandler=(val)=>{
-    setNotebook(notebook+val)
+  const notebookHandler = (val) => {
+    setNotebook(notebook + val);
   };
-  const inkpenHandler=(val)=>{
-    setInkpen(inkpen+val)
+  const inkpenHandler = (val) => {
+    setInkpen(inkpen + val);
   };
 
   return (
-    <div className="items">
-    <span>Books:</span>
-    <button className="addBook" onClick={() =>{
-      bookHandler(1);
-    }}>
-        +
-    </button>
-    <button className="remBook" onClick={()=>{
-      if(book>=1){
-        bookHandler(-1);
-      }
-    }}>
-        -
-    </button>
-    <span className="totalBooks">{book}</span>
-    <div className="items">
-    <span>Pens:</span>
-    <button className="addPen" onClick={() =>{
-      penHandler(1);
-    }}>
-        +
-    </button>
-    <button className="remPen" onClick={()=>{
-      if(pen>=1){
-        penHandler(-1);
-      }
-    }}>
-        -
-    </button>
-    <span className="totalPens">{pen}</span>
-</div>
-<div className="items">
-    <span>Notebooks:</span>
-    <button className="addNotebook" onClick={() =>{
-      notebookHandler(1);
-    }}>
-        +
-    </button>
-    <button className="remNotebook" onClick={()=>{
-      if(notebook>=1){
-        notebookHandler(-1);
-      }
-    }} >
-        -
-    </button>
-    <span className="totalNotebooks">{notebook}</span>
-</div>
-<div className="items">
-    <span>Inkpens:</span>
-    <button className="addInkpen" onClick={() =>{
-      inkpenHandler(1);
-    }}>
-        +
-    </button>
-    <button className="remInkpen" onClick={()=>{
-      if(inkpen>=1){
-        inkpenHandler(-1);
-      }
-    }}>
-        -
-    </button>
-    <span className="totalInkpens">{inkpen}</span>
-    <div className="total">{book+pen+notebook+inkpen}</div>
-</div>
-</div>
+    <div className="main">
+      <div className="items">
+        <span>Books:</span>
+        <button
+          className="addBook"
+          onClick={() => {
+            bookHandler(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="remBook"
+          onClick={() => {
+            if (book >= 1) {
+              bookHandler(-1);
+            }
+          }}
+        >
+          -
+        </button>
+        <span className="totalBooks">{book}</span>
+      </div>
 
+      <div className="items">
+        <span>Pens:</span>
+        <button
+          className="addPen"
+          onClick={() => {
+            penHandler(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="remPen"
+          onClick={() => {
+            if (pen >= 1) {
+              penHandler(-1);
+            }
+          }}
+        >
+          -
+        </button>
+        <span className="totalPens">{pen}</span>
+      </div>
+      <div className="items">
+        <span>Notebooks:</span>
+        <button
+          className="addNotebook"
+          onClick={() => {
+            notebookHandler(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="remNotebook"
+          onClick={() => {
+            if (notebook >= 1) {
+              notebookHandler(-1);
+            }
+          }}
+        >
+          -
+        </button>
+        <span className="totalNotebooks">{notebook}</span>
+      </div>
+
+      <div className="items">
+        <span>Inkpens:</span>
+        <button
+          className="addInkpen"
+          onClick={() => {
+            inkpenHandler(1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className="remInkpen"
+          onClick={() => {
+            if (inkpen >= 1) {
+              inkpenHandler(-1);
+            }
+          }}
+        >
+          -
+        </button>
+        <span className="totalInkpens">{inkpen}</span>
+      </div>
+
+      <div className="total">{book + pen + notebook + inkpen}</div>
+    </div>
   );
 }
 
